04c55460793c4309b7204423a126f1bb6f5fd1a9
hjaneiro001 <hjaneiro@lefkas.com.ar>
2024-10-14 20:56:22 -0300

    resulevo comentrios

--- app/repositories/productRepository.py
+++ app/repositories/productRepository.py
@@ -11,42 +11,6 @@ class ProductRepository:
     def __init__(self, connection):
         self.conn = connection
 
-    # def get_all(self):
-    #     product1 = (
-    #         ProductBuilder()
-    #         .product_id(1)
-    #         .code("ProductCode")
-    #         .bar_code("BarCode")
-    #         .name("FAKE Product")
-    #         .description("Description")
-    #         .pack(1)
-    #         .price(1)
-    #         .currency(Currency.ARS)
-    #         .iva(ProductIva.I21)
-    #         .product_type(ProductType.BC)
-    #         .status(ClientStatus.ACTIVE)
-    #         .build()
-    #     )
-    #
-    #     product2 = (
-    #         ProductBuilder()
-    #         .product_id(2)
-    #         .code("ProductCode2")
-    #         .bar_code("BarCode2")
-    #         .name("FAKE Product 2")
-    #         .description("Description 2")
-    #         .pack(2)
-    #         .price(2)
-    #         .currency(Currency.ARS)
-    #         .iva(ProductIva.I21)
-    #         .product_type(ProductType.BC)
-    #         .status(ClientStatus.ACTIVE)
-    #         .build()
-    #     )
-    #
-    #     return [product1, product2]
-    #
-
     def get_all(self):
 
             sql = "SELECT * FROM Products WHERE product_status = 'ACTIVE'"
