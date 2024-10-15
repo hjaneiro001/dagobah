1a442c5cd1df3db0c85db14cdcb23eab3b2302ed
hjaneiro001 <hjaneiro@lefkas.com.ar>
2024-10-15 15:20:44 -0300

    test para cntrollaer product get_all terminado

---
+++ test/mothers/productMother.py
@@ -0,0 +1,23 @@
+from app.entities.product import Product, ProductBuilder
+from app.entities.enums.clientStatus import ClientStatus
+from app.entities.enums.productType import  ProductType
+from app.entities.enums.productIva import ProductIva
+from app.entities.enums.currency import Currency
+
+class ProductMother:
+    @staticmethod
+    def normal_product(product_id: int):
+        product: Product = (ProductBuilder()
+                            .product_id(product_id)
+                            .code("P001")
+                            .bar_code("1234567890123")
+                            .name("Sample Product")
+                            .description("This is a sample product.")
+                            .pack(1.0)
+                            .price(9.99)
+                            .currency(Currency.ARS)
+                            .iva(ProductIva.I21)
+                            .product_type(ProductType.BC)
+                            .status(ClientStatus.ACTIVE)
+                            .build())
+        return product
