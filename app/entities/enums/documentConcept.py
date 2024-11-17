52762f2a84c75e09412d527912c08ce028bbe0c0
hjaneiro001 <hjaneiro@lefkas.com.ar>
2024-11-17 17:08:51 -0300

    document and iten entities terminadas

---
+++ app/entities/enums/documentConcept.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class DocumentConcept(Enum):
+    BI = ['BIENES', 1]
+    SE = ['SERVICIOS', 2]
+    BS = ['BIENES Y SERVICIOS', 3]
+
+    def get_documentconcept(value):
+        for item in DocumentConcept:
+            if value in item.value:
+                return item
+        return None
+
+    def get_concept(self):
+        return self.value[0]
+
+
+    def get_value(self):
+        return self.value[1]
---
+++ app/entities/enums/documentType.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class DocumentType(Enum):
+    FACTURA = ['FACTURA', 0]
+    NOTADECREDITO = ['NOTA DE CREDITO', 1]
+
+    def get_documenttype(value):
+        for item in DocumentType:
+            if value in item.value:
+                return item
+        return None
+
+    def get_type(self):
+        return self.value[0]
+
+
+    def get_value(self):
+        return self.value[1]
